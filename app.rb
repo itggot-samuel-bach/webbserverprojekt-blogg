393a65f4a4d34c578a2fa43e688b1f647c4bb0b4
Ove Sundberg <samuel.bach@itggot.se>
2019-03-12 14:35:31 +0100

    Create account, login and logout
    Fått de 3 ovanstående att fungera

--- app.rb
+++ app.rb
@@ -1,9 +1,53 @@
 require "sinatra"
 require "slim"
 require "sqlite3"
+require "bcrypt"
 
+enable :sessions
 
-get('/') do 
-    slim(:index)
+get('/') do
+    slim(:index, locals:{session: session})
 end
 
+get('/denied') do
+    slim(:denied)
+end
+
+get('/accepted') do
+    slim(:accepted, locals:{session: session})
+end
+
+post('/login') do
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    
+    result = db.execute("SELECT id, password FROM users WHERE username=?", [params["username"]])
+    if result.length == 0
+        redirect('/denied')
+    end  
+    if BCrypt::Password.new(result[0]["password"]) == params["password"]
+        session["user"] = result[0]['id']
+        redirect('/accepted')
+    else
+        redirect('/denied')
+    end
+end 
+
+post('/create') do
+    #Ansluta till db
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    hashed_password = BCrypt::Password.create(params["passwordC"])
+    db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [params["usernameC"], hashed_password])
+
+    redirect('/')
+end
+
+get('/profile/:id') do
+    slim(:user)
+end
+
+post('/logout') do
+    session.destroy
+    slim(:index)
+end
