61cb0f623349254f3078da3471eecbddf5af7381
Ove Sundberg <samuel.bach@itggot.se>
2019-03-14 12:04:00 +0100

    Fixade log in

--- app.rb
+++ app.rb
@@ -2,11 +2,15 @@ require "sinatra"
 require "slim"
 require "sqlite3"
 require "bcrypt"
-
+require 'securerandom'
 enable :sessions
 
 get('/') do
-    slim(:index, locals:{session: session})
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    
+    result = db.execute('SELECT * FROM posts')
+    slim(:index, locals:{posts: result, session: session})
 end
 
 get('/denied') do
@@ -44,10 +48,39 @@ post('/create') do
 end
 
 get('/profile/:id') do
-    slim(:user)
+    if session["user"] == params[:id].to_i
+        slim(:user)
+    else
+        redirect('/denied')
+    end
 end
 
 post('/logout') do
-    session.destroy
-    slim(:index)
+    session.clear
+    redirect('/')
+end
+
+post('/post') do
+    text = params["content"]
+    db = SQLite3::Database.new('db/database.db')
+
+    username = db.execute("SELECT username FROM users WHERE id=?", [session["user"]])
+
+    new_file_name = SecureRandom.uuid
+    temp_file = params["image"]["tempfile"]
+    path = File.path(temp_file)
+    
+    new_file = FileUtils.copy(path, "./public/img/#{new_file_name}")
+
+    db.execute('INSERT INTO posts (content, picture, userId) VALUES (?, ?, ?)', [text, new_file_name, username])
+
+    redirect('/')
+
+end
+
+post('/delete') do
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+
+    
 end
