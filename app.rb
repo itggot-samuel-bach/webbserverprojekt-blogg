51f0cb8279ea260b479aeefa655b2cc71a525b04
Ove Sundberg <samuel.bach@itggot.se>
2019-03-17 14:59:40 +0100

    Färdig
    Färdigställde sidan.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "slim"
 require "sqlite3"
 require "bcrypt"
 require 'securerandom'
+
 enable :sessions
 
 get('/') do
@@ -10,6 +11,7 @@ get('/') do
     db.results_as_hash = true
     
     result = db.execute('SELECT * FROM posts')
+
     slim(:index, locals:{posts: result, session: session})
 end
 
@@ -31,6 +33,7 @@ post('/login') do
     end  
     if BCrypt::Password.new(result[0]["password"]) == params["password"]
         session["user"] = result[0]['id']
+        session["username"] = params["username"]
         redirect('/accepted')
     else
         redirect('/denied')
@@ -48,11 +51,40 @@ post('/create') do
 end
 
 get('/profile/:id') do
-    if session["user"] == params[:id].to_i
-        slim(:user)
+
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+
+    result = db.execute('SELECT * FROM posts WHERE userId=?', params["id"])
+    
+    slim(:user, locals:{posts: result, session: session, user: params["id"]})
+
+end
+
+get('/profile/:id/edit') do
+
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    
+    result = db.execute('SELECT * FROM users WHERE username=?', session["username"])
+
+    if session["username"] == params["id"]
+        slim(:user_edit, locals:{user: result[0], session: session})
     else
-        redirect('/denied')
+        slim(:denied)
     end
+
+end
+
+post('/profile/:id/edit') do 
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
+    hashed_password = BCrypt::Password.create(params["password"])
+    db.execute("REPLACE INTO users (id, username, password) VALUES (?, ?, ?)", [params["user_id"], params["username"], hashed_password])
+
+    redirect('/')
+
+
 end
 
 post('/logout') do
@@ -82,5 +114,29 @@ post('/delete') do
     db = SQLite3::Database.new('db/database.db')
     db.results_as_hash = true
 
+    db.execute("DELETE FROM posts WHERE id = ?", params["post_id"])
+
+    redirect('/')
+end
+
+get('/edit/:id') do
+    slim(:edit, locals:{id: params["id"]})
+end
+
+post('/edit_post') do
+    text = params["content"]
+    post_ident = params["post_id"]
+    db = SQLite3::Database.new('db/database.db')
+    db.results_as_hash = true
     
+    new_file_name = SecureRandom.uuid
+    temp_file = params["image"]["tempfile"]
+    path = File.path(temp_file)
+    
+    new_file = FileUtils.copy(path, "./public/img/#{new_file_name}")
+
+    db.execute('REPLACE INTO posts (id, content, picture, userId) VALUES (?, ?, ?, ?)', [post_ident, text, new_file_name, session["username"]])
+   
+    redirect('/')
+
 end
